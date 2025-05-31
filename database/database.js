b3df352850c6905681a70349622e3fe5fb7a1071
dinoAtomic0001 <dinoatomicz@gmail.com>
2025-06-01 03:37:52 +0530

    V2 -  Added all functions

--- database/database.js
+++ database/database.js
@@ -38,19 +38,20 @@ pool.connect()
 
 
 
-async function createUser(email, phoneNumber) {
+async function createUser(email, phoneNumber, linkPrecedence, linkedid) {
   const query = `
     INSERT INTO user_contacts (
         email,
         phoneNumber,
-        linkPrecedence)
+        linkPrecedence,
+        linkedId)
         VALUES
         (
-        $1, $2, $3
+        $1, $2, $3, $4
         )
          RETURNING *;
     `;
-  const values = [ email,phoneNumber, 'primary'];
+  const values = [ email,phoneNumber, linkPrecedence, linkedid];
   const result = await pool.query(query, values);
   console.log('✅ User inserted with ID:', result.rows[0].id);
   return result.rows;  // return the inserted row
@@ -60,11 +61,11 @@ async function createUser(email, phoneNumber) {
 async function checkLinkedid(userDetails) {
   let primaryContact = [];
 
-  for (let i = 0; i < userDetails.length; i++) {
+  for(let i = 0; i < userDetails.length; i++) {
 
     let primId = userDetails[i].linkedid;
-    // console.log(primId);
-    if (primId) {
+
+    if (primId != null) {
       let primary = await pool.query(
         `
 SELECT * FROM user_contacts
@@ -74,11 +75,65 @@ WHERE
 `,
         [primId]
       );
+
       primaryContact.push(primary.rows[0]);
+
+      
     }
+    
+
+    
+
+    
+    
   }
+  
   return primaryContact;
 }
+
+
+
+
+
+async function update(linkedidContact, userDetails) {
+    if (linkedidContact.length > 0) {
+       let highTimeStamp ,lowestTimeStamp;
+  highTimeStamp = linkedidContact[0].createdat;
+  let primaryUserContact =  [linkedidContact[0]];
+  let primaryId = primaryUserContact[0].id;
+  for (let i = 0; i < linkedidContact.length; i++) {
+ 
+  if(linkedidContact[i] != null && await userDetails.find(Obj => Obj.id != linkedidContact[i].id) ){ 
+    
+  if (linkedidContact[i].createdat > highTimeStamp) {
+    
+    
+    highTimeStamp = linkedidContact[i].createdat;
+    primaryUserContact = linkedidContact[i];
+    // primaryId = linkedidContact[i].id;
+    const update = await pool.query(
+      `
+      UPDATE user_contacts set linkedId = $1, linkPrecedence = $2, updatedAt = CURRENT_TIMESTAMP 
+      where id = $3
+
+      `,[primaryId , "secondary", linkedidContact[i].id]
+    )
+ 
+  }
+
+    
+
+    userDetails.push(linkedidContact[i]);
+  }
+  
+  }
+  }
+}
+
+
+
+
+
 
 
 async function findOrCreateUser({ email, phoneNumber }) {
@@ -93,35 +148,140 @@ WHERE
 `,
     [email, phoneNumber]
   );
+
+
+
+
+  if (email && !phoneNumber ) {
+      const emailResult = await pool.query(
+    `
+SELECT * FROM user_contacts
+WHERE 
+  (phonenumber = $1 AND $1 != '' );
+
+`,
+    [result.rows[0].phonenumber]
+  );
+  
+  for (let i = 0; i < emailResult.rows.length; i++) {
+    result.rows.push(emailResult.rows[i]);
+    
+  }
+
+  
+  }
+
+  if (!email && phoneNumber) {
+      const phoneResult = await pool.query(
+    `
+SELECT * FROM user_contacts
+WHERE 
+  (email = $1 AND $1 != '' );
+
+`,
+    [result.rows[0].email]
+  );
+    for (let i = 0; i < phoneResult.rows.length; i++) {
+    result.rows.push(phoneResult.rows[i]);
+    
+  }
+  }
+
+
   if (result.rows.length === 0) {
-    const t = await createUser(email, phoneNumber);
+    const t = await createUser(email, phoneNumber, "primary", null);
     return t;
   }
+
+
+
+
+
 
 const checkEmail = await result.rows.find(Obj => Obj.email === email);
 const checkPhoneNumber = await result.rows.find(Obj => Obj.phonenumber === phoneNumber);
 const userDetails = result.rows;
-console.log(checkEmail);
-console.log(checkPhoneNumber );
- 
 
 
 
-if(checkEmail === undefined){
-  let  newUser = await createUser( email, '');
-  result.rows.push(newUser);
+
+
+if(!checkEmail && checkPhoneNumber && email != ""){
+  let  newUser = await createUser( email, phoneNumber, "secondary", checkPhoneNumber.id);
+
+  result.rows.push(newUser[0]);
 }
-if (checkPhoneNumber === undefined) {
- let  newUser = await createUser( '', phoneNumber);
-result.rows.push(newUser);
+if (!checkPhoneNumber  && checkEmail && phoneNumber !="") {
+ let  newUser = await createUser( email, phoneNumber, "secondary", checkEmail.id);
+  result.rows.push(newUser[0]);
 }
+
 
 
   const linkedidContact = await checkLinkedid(userDetails);
+ if (linkedidContact) {
+  await update(linkedidContact, userDetails);
+ }
 
-  userDetails.push(linkedidContact[0]);
+  
+  
 
-  return userDetails;
+
+
+
+let thirdRelation = [];
+for (let i = 0; i < userDetails.length; i++) {
+
+  if (userDetails[i].linkprecedence === "primary") {
+    thirdRelation.push(userDetails[i]);
+
+  }
+  
+}
+
+
+update(thirdRelation, userDetails)
+
+
+
+
+
+const primaryIDs = [];
+
+for (let i = 0; i < userDetails.length; i++) {
+    if (userDetails[i].linkprecedence === "primary" && !primaryIDs.includes(userDetails[i].id)) {
+      primaryIDs.push(userDetails[i].id);
+    }
+}
+
+const finalDetails = await pool.query(
+
+  `
+  SELECT * FROM user_contacts WHERE
+  linkedid = ANY($1::int[]);
+  `,[primaryIDs]
+
+)
+
+// userDetails.push(finalDetails.rows);
+
+
+
+const final_result = [ ...userDetails, ...finalDetails.rows ];
+
+const resu = final_result.reduce((uniqueUsers, current) => {
+  if (!uniqueUsers.find(item => item.id === current.id)) {
+    uniqueUsers.push(current);
+  }
+  return uniqueUsers; // ← THIS was missing
+}, []);
+
+
+
+  console.log(resu);
+  
+
+  return resu;
 
 }
 
--- routes/indentify.js
+++ routes/indentify.js
@@ -9,9 +9,10 @@ router.post('/identify', async(req, res) => {
 	const email = req.body.email.trim();
 	const phoneNumber = req.body.phoneNumber.trim();
 	
-if (!email && !phoneNumber) {
+if (!email && !phoneNumber ) {
     return res.status(400).json({ error: 'Email or phoneNumber required' });
   }
+
 
 
   try {
@@ -22,17 +23,36 @@ if (!email && !phoneNumber) {
       const emails = [];
       const phoneNumbers = [];
       const secondaryContactIds = [];
+      let primaryID = []; 
+     
       
-    for (let i = 0; i < user.length; i++) {
+      
+ function constructResult(user) {
+      for (let i = 0; i < user.length; i++) {
       const element = user[i];
 
+      if(element.linkprecedence === "primary"){
+        primaryID.push(element.id);
+      }
+      if(emails.includes(element.email) === false){
+        emails.push(element.email);
+      }
+       if(phoneNumbers.includes(element.phonenumber) === false){
+        phoneNumbers.push(element.phonenumber);
+      }
+      
+      if(element.linkedid != null && element.linkprecedence === "secondary" && secondaryContactIds.includes(element.id) == false ){
+        secondaryContactIds.push(element.id);
+      }
       
     }
+}
 
+  constructResult(user);
 
-    const result = {
+    const result =  {
       "contact":{
-         	"primaryContatctId": 11,
+      "primaryContatctId": primaryID[0],
 			"emails": emails, // first element being email of primary contact 
 			"phoneNumbers": phoneNumbers, // first element being phoneNumber of primary contact
 			"secondaryContactIds": secondaryContactIds // Array of all Contact IDs that are "secondary" to the primary contact  
@@ -40,10 +60,10 @@ if (!email && !phoneNumber) {
     }
 
 
-    res.send(user);
+    res.send(result);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: 'DATA NOT FOUND - Enter both email and phone number to create a new account, After creating account you can add both the details or either of them to complete your order' });
   }
 
 
